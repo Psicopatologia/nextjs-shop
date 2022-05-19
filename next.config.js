1e8b2aa0af838f7d863c09ed621b1cbd57bcd12b
Sebastián Orozco <sebastianorozcogomez@gmail.com>
2022-05-19 02:41:49 -0500

    Created base project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue'
+  },
+  /* basePath: '/dist', */
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: '/',
+        permanent: true
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
